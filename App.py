36ac59e35d181ce6dc56a4466d4e6d27160383e2
Jw-trindade <trindade2888@gmail.com>
2026-07-11 23:17:27 -0300

    criar rotas principais e primeira versão dos templates HTML
    - Configura o arquivo 'app.py' com as rotas para a Home e Calculadora
    - Cria o HTML da Landing Page ('Index.html') estruturado com divs e botão de navegação
    - Cria o HTML da Calculadora ('Calculadora.html') estruturado com divs e botão de retorno

--- App.py
+++ App.py
@@ -1,1 +1,14 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("Index.html")
+
+@app.route("/Calculadora")
+def calculadora():
+    return render_template("Calculadora.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
